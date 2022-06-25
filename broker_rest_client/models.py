407bb6e15870477c477d605cf5b1a80dcdde044c
kjenod <107796030+kjenod@users.noreply.github.com>
2022-06-25 21:05:52 +0200

    Add files via upload

--- broker_rest_client/models.py
+++ broker_rest_client/models.py
@@ -64,7 +64,7 @@ class RabbitMQUser(BaseModel):
     @classmethod
     def from_json(cls, user_dict):
         print ("--------------------- user_dict ", user_dict)
-        print ("--------------------- user_dict['name'] " , user_dict['nam']  )
+        print ("--------------------- user_dict['name'] " , user_dict['name']  )
         print ("--------------------- user_dict['tags'] " , user_dict['tags'] )
         print ("--------------------- user_dict['tags'].split(',') ", user_dict['tags'].split(',') )
         return cls(
